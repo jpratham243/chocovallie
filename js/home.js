e2fd3d645c23e453e782b6eb87d9506624779f7f
jpratham243 <142526449+jpratham243@users.noreply.github.com>
2024-09-26 23:34:57 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,23 @@
+let swiperHome = new Swiper(".top-seller-swiper", {
+  loop: true,
+  spaceBetween: 0,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+});
+
+let testimonialSwiper = new Swiper(".testimonial-swiper ", {
+  loop: true,
+  spaceBetween: 0,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+});
